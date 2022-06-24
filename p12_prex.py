59b61082b958be380798c4e4337617a0c1d2afa5
Ritish Shelke <89585029+Ritishshelke007@users.noreply.github.com>
2022-06-24 08:46:00 +0530

    Add files via upload

---
+++ p12_prex.py
@@ -0,0 +1,10 @@
+
+def calculateDiscount(quantity, amount):
+    dis = 10
+    discount = quantity*amount*dis/100
+    return discount
+
+
+
+# dis_amount = calculateDiscount(1200,300)
+# print('Discounted amount is : ',dis_amount)
---
+++ test_module.py
@@ -0,0 +1,4 @@
+import p12_prex
+
+dis_amount = p12_prex.calculateDiscount(1200,300)
+print(dis_amount)
